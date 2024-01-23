fa79739cf872906ba7a99f5e87282c7fa128b874
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-01-23 17:13:49 +0200

    fix: change code to avoid sync functions

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs').promises;
+const fsPromises = require('fs').promises;
 const path = require('path');
 
 const sourceFolder = './06-build-page';
@@ -14,53 +15,51 @@ const replaceTemplateTags = async (templatePath, componentsPath) => {
 
     let updatedContent = templateContent;
 
-    await Promise.all(
-      componentFiles.map(async (componentFile) => {
-        const componentName = path.parse(componentFile).name;
-        const componentPath = path.join(componentsPath, componentFile);
-        const componentContent = await fs.readFile(componentPath, 'utf8');
-        const templateTag = `{{${componentName}}}`;
+    for (const componentFile of componentFiles) {
+      const componentName = path.parse(componentFile).name;
+      const componentPath = path.join(componentsPath, componentFile);
+      const componentContent = await fs.readFile(componentPath, 'utf8');
+      const templateTag = `{{${componentName}}}`;
 
-        updatedContent = updatedContent.replace(
-          new RegExp(templateTag, 'g'),
-          componentContent,
-        );
-      }),
-    );
+      updatedContent = updatedContent.replace(
+        new RegExp(templateTag, 'g'),
+        componentContent,
+      );
+    }
 
     return updatedContent;
   } catch (error) {
-    console.error(`Error replacing template tags: ${error.message}`);
-    return '';
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
   }
 };
 
 const compileStyles = async (source, destination, outputFileName) => {
   try {
-    const files = await fs.readdir(source);
+    const files = await fsPromises.readdir(source);
     const cssFiles = files.filter(
       (file) => path.extname(file).toLowerCase() === '.css',
     );
-    const writeStream = fs.createWriteStream(
-      path.join(destination, outputFileName),
+
+    const outputFile = path.join(destination, outputFileName);
+
+    let cssContent = '';
+
+    for (const file of cssFiles) {
+      const filePath = path.join(source, file);
+      const fileContent = await fsPromises.readFile(filePath, 'utf8');
+
+      cssContent += `/* ${file} */\n${fileContent}\n\n`;
+    }
+
+    await fsPromises.writeFile(outputFile, cssContent);
+
+    console.log(
+      `CSS compilation completed. Check ${outputFileName} in ${destination}.`,
     );
-
-    await Promise.all(
-      cssFiles.map(async (file) => {
-        const filePath = path.join(source, file);
-        const fileContent = await fs.readFile(filePath, 'utf8');
-
-        writeStream.write(`/* ${file} */\n${fileContent}\n\n`);
-      }),
-    );
-
-    writeStream.end(() => {
-      console.log(
-        `CSS compilation completed. Check ${outputFileName} in ${destination}.`,
-      );
-    });
   } catch (error) {
-    console.error(`Error compiling styles: ${error.message}`);
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
   }
 };
 
@@ -68,61 +67,41 @@ const copyAssets = async (source, destination) => {
   try {
     const assetsPath = path.join(destination, 'assets');
 
-    // Check if the destination folder exists
-    try {
-      await fs.access(assetsPath);
-    } catch (error) {
-      // If the destination folder does not exist, create it
-      await fs.mkdir(assetsPath, { recursive: true });
+    await fsPromises.mkdir(assetsPath, { recursive: true });
+
+    const files = await fsPromises.readdir(source);
+
+    for (const item of files) {
+      const sourcePath = path.join(source, item);
+      const destinationPath = path.join(assetsPath, item);
+
+      const isDirectory = (await fsPromises.stat(sourcePath)).isDirectory();
+
+      if (isDirectory) {
+        await copyAssets(sourcePath, destinationPath);
+      } else {
+        const fileContent = await fsPromises.readFile(sourcePath);
+
+        await fsPromises.writeFile(destinationPath, fileContent);
+      }
     }
-
-    const files = await fs.readdir(source);
-
-    // Copy each file or directory from the source to the destination assets folder
-    await Promise.all(
-      files.map(async (item) => {
-        const sourcePath = path.join(source, item);
-        const destinationPath = path.join(assetsPath, item);
-
-        const stats = await fs.stat(sourcePath);
-
-        if (stats.isDirectory()) {
-          await copyAssets(sourcePath, destinationPath);
-        } else {
-          const readStream = fs.createReadStream(sourcePath);
-          const writeStream = fs.createWriteStream(destinationPath);
-
-          readStream.pipe(writeStream);
-
-          readStream.on('error', (error) => {
-            console.error(`Error reading asset file ${item}: ${error.message}`);
-          });
-
-          writeStream.on('error', (error) => {
-            console.error(
-              `Error writing asset file ${item} to ${assetsPath}: ${error.message}`,
-            );
-          });
-        }
-      }),
-    );
 
     console.log('Asset copying completed.');
   } catch (error) {
-    console.error(`Error copying assets: ${error.message}`);
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
   }
 };
 
 const buildProject = async () => {
   try {
-    if (!(await fs.exists(destinationFolder))) {
-      await fs.mkdir(destinationFolder);
-    }
+    await fs.mkdir(destinationFolder, { recursive: true });
 
     const updatedHtmlContent = await replaceTemplateTags(
       path.join(sourceFolder, templateFileName),
       path.join(sourceFolder, 'components'),
     );
+
     await fs.writeFile(
       path.join(destinationFolder, outputHtmlFileName),
       updatedHtmlContent,
@@ -138,7 +117,8 @@ const buildProject = async () => {
 
     console.log('Project building completed.');
   } catch (error) {
-    console.error(`Error building project: ${error.message}`);
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
   }
 };
 
