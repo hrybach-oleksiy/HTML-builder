1312303295025c77bd6a4717811eadb6782be725
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-01-23 16:35:22 +0200

    fix: delete sync methods

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,47 +1,52 @@
 const fs = require('fs');
 const path = require('path');
 
-const sourceFolder = './05-merge-styles/styles';
-const destinationFolder = './05-merge-styles/project-dist';
-const outputFile = 'bundle.css';
+const stylesDir = path.join(__dirname, 'styles');
+const outputDir = path.join(__dirname, 'project-dist');
+const outputFile = path.join(outputDir, 'bundle.css');
 
-const compileStyles = (source, destination, outputFileName) => {
-  // Read the contents of the source directory
-  fs.readdir(source, (err, files) => {
+const compileCSSBundle = () => {
+  fs.mkdir(outputDir, { recursive: true }, (err) => {
     if (err) {
-      console.error(`Error reading source directory: ${err.message}`);
+      console.error(`Error creating output directory: ${err.message}`);
       return;
     }
 
-    const cssFiles = files.filter(
-      (file) => path.extname(file).toLowerCase() === '.css',
-    );
+    fs.readdir(stylesDir, { withFileTypes: true }, (err, files) => {
+      if (err) {
+        console.error(`Error reading styles directory: ${err.message}`);
+        return;
+      }
 
-    if (!fs.existsSync(destination)) {
-      fs.mkdirSync(destination);
-    }
-
-    const writeStream = fs.createWriteStream(
-      path.join(destination, outputFileName),
-    );
-
-    cssFiles.forEach((file) => {
-      const filePath = path.join(source, file);
-      const fileContent = fs.readFileSync(filePath, 'utf8');
-
-      writeStream.write(`/* ${file} */\n${fileContent}\n\n`, (writeErr) => {
-        if (writeErr) {
-          console.error(`Error writing to output file: ${writeErr.message}`);
-        }
-      });
-    });
-
-    writeStream.end(() => {
-      console.log(
-        `Compilation completed. Check ${outputFileName} in ${destination}.`,
+      const cssFiles = files.filter(
+        (file) => file.isFile() && path.extname(file.name) === '.css',
       );
+
+      let cssBundle = '';
+
+      cssFiles.forEach((file, index, array) => {
+        const filePath = path.join(stylesDir, file.name);
+
+        fs.readFile(filePath, 'utf8', (err, fileContent) => {
+          if (err) {
+            console.error(`Error reading file: ${err.message}`);
+          } else {
+            cssBundle += `${fileContent}\n`;
+
+            if (index === array.length - 1) {
+              fs.writeFile(outputFile, cssBundle, (err) => {
+                if (err) {
+                  console.error(`Error writing CSS bundle: ${err.message}`);
+                } else {
+                  console.log('CSS bundle created successfully.');
+                }
+              });
+            }
+          }
+        });
+      });
     });
   });
 };
 
-compileStyles(sourceFolder, destinationFolder, outputFile);
+compileCSSBundle();
